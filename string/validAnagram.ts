ff089fa2eede2dd4831536ef7d4ee91296350121
jtrieu1998 <43070404+jtrieu1998@users.noreply.github.com>
2025-04-10 14:50:14 -0500

    Update validAnagram.ts

--- string/validAnagram.ts
+++ string/validAnagram.ts
@@ -1,3 +1,7 @@
 function isAnagram(s: string, t: string): boolean {
     return (s.split('').sort().join() == t.split('').sort().join())
 };
+
+// if not allowed to use built in string functions then solution would be:
+// use a hashtable with counts of each letter for both strings
+// loop through a string and make sure hashtables match
